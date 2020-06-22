9c7afcf519fb7b18a8bc1d611e563313aa3b0c35
Gabriel Fontes <g.fontes@usp.br>
2020-06-22 17:31:13 -0300

    Fixed help text wrapping at wrong width. Also fleshed out pattern grabbing a bit

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,46 @@ extern crate clap;
 use clap::App;
 
 fn main() {
+    //Load yml file
     let yaml = load_yaml!("cli.yml");
-    let m = App::from_yaml(yaml)
+    //Instanciate clap app from yml and add author and version info
+    let matches = App::from_yaml(yaml)
         .author(crate_authors!())
         .version(crate_version!())
         .get_matches();
 
-    match m.subcommand() {
-        ("apply",  Some(sub_m)) => {
-            println!("apply");
+
+    //Check which subcommand was used
+    match matches.subcommand() {
+        //Apply subcommand
+        ("apply",  Some(sub_matches)) => {
+            //If pattern isn't specified, grab last applied scheme
+            let pattern = sub_matches.value_of("pattern")
+                          .unwrap_or_else(|| "placeholder");
+            println!("{}", pattern);
         },
-        ("query",  Some(sub_m)) => {
-            println!("query");
+        //Query subcommand
+        ("query",  Some(sub_matches)) => {
+            //If pattern isn't specified, grab last applied scheme
+            let pattern = sub_matches.value_of("pattern")
+                          .unwrap_or_else(|| "placeholder");
+            println!("{}", pattern);
         },
-        ("update", Some(sub_m)) => {
-            println!("update");
+        //Update subcommand
+        ("update", Some(sub_matches)) => {
+            //Check which update operation was used
+            //(We can safely unwrap, clap handles errors or missing arguments)
+            match sub_matches.value_of("operation").unwrap() {
+                //All operation
+                "all" => {},
+                //Lists operation
+                "lists" => {},
+                //Schemes
+                "schemes" => {},
+                //Template
+                "templates" => {},
+                _ => {},
+            }
         },
         _ => {},
     }
