d2be3d133e20214c3e5208d84f75cb6dbf55fc1b
Loic Coyle <loic.coyle@hotmail.fr>
2022-03-03 20:10:00 +0100

    feat: look for schemes and templates in config dir
    flavours will also look for schemes & templates in the user's config dir, e.g.:
    
    * scheme: `~/.config/flavours/schemes/myscheme/myscheme.yaml`
    
    * template: `~/.config/flavours/templates/mysoftware/templates/mytemplate.mustache`

--- src/find.rs
+++ src/find.rs
@@ -1,24 +1,63 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use glob::glob;
 use path::{Path, PathBuf};
 use std::path;
 
-/// Find function
+/// Find color schemes matching pattern in either the config dir or the data dir.
 ///
 /// * `pattern` - Which pattern to use
-/// * `schemes_dir` - Schemes directory
-pub fn find(pattern: &str, schemes_dir: &Path) -> Result<Vec<PathBuf>> {
-    let dir = schemes_dir
-        .to_str()
-        .ok_or_else(|| anyhow!("Unable to convert path"))?;
-
-    let pattern = format!("{}/*/{}.y*ml", dir, pattern);
-    let matches = glob(&pattern)?;
+/// * `base_dir` - flavours' base data dir
+/// * `config_dir` - flavours' config dir
+pub fn find_schemes(pattern: &str, base_dir: &Path, config_dir: &Path) -> Result<Vec<PathBuf>> {
+    let config_scheme_dir = config_dir.join("schemes");
+    let data_scheme_dir = base_dir.join("base16").join("schemes");
+    let dir_vec = vec![config_scheme_dir, data_scheme_dir];
+    let dir_vec: Vec<&str> = dir_vec.iter().filter_map(|dir| dir.to_str()).collect();
 
     let mut found = Vec::new();
-    for element in matches {
-        found.push(element?);
+    for dir in dir_vec {
+        let glob_pattern = format!("{}/*/{}.y*ml", dir, pattern);
+        let matches = glob(&glob_pattern)?;
+        for element in matches {
+            found.push(element?);
+        }
     }
-
     Ok(found)
+}
+
+/// Find template file in either the config dir or the data dir.
+///
+/// * `template` - template
+/// * `subtemplate` - subtemplate
+/// * `base_dir` - flavours' base data dir
+/// * `config_dir` - flavours' config dir
+pub fn find_template(
+    template: &str,
+    subtemplate: &str,
+    base_dir: &Path,
+    config_dir: &Path,
+) -> Result<PathBuf> {
+    let template_config_file = config_dir
+        .join("templates")
+        .join(&template)
+        .join("templates")
+        .join(format!("{}.mustache", subtemplate));
+
+    let template_data_file = base_dir
+        .join("base16")
+        .join("templates")
+        .join(&template)
+        .join("templates")
+        .join(format!("{}.mustache", subtemplate));
+
+    if template_config_file.is_file() {
+        Ok(template_config_file)
+    } else if template_data_file.is_file() {
+        Ok(template_data_file)
+    } else {
+        panic!(
+            "Neither {:?} or {:?} exist",
+            template_config_file, template_data_file
+        )
+    }
 }
